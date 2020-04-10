67678efc06ea6af96be52a8a0cd79cdac0bc427b
Nuttapon Buaban <50200295+9ine2543@users.noreply.github.com>
2020-04-10 13:26:05 +0700

    Add feature get current location

--- lib/pages/addHomeLocation.dart
+++ lib/pages/addHomeLocation.dart
@@ -39,61 +39,86 @@ class _addHomeLocationState extends State<addHomeLocation> {
     
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.only(top: 50, bottom: 30, left: 30, right: 30),
-        child: Stack(
-          children: <Widget>[
-            GoogleMap(
-              mapType: MapType.normal,
-              rotateGesturesEnabled: false,
-              scrollGesturesEnabled: true,
-              tiltGesturesEnabled: false,
-              zoomGesturesEnabled: true,
-              // markers: {
-              //   Marker(
-              //     markerId: MarkerId('HomeMarker'),
-              //     position: LatLng(latitude, longitude)
-              //     )
-              // },
-              initialCameraPosition: CameraPosition(
-                target: LatLng(_currentPosition.latitude , _currentPosition.longitude),
-                zoom: 18,
-              ),
-              onMapCreated: (GoogleMapController controller) {
-                _controller = controller;
-              },
-              onCameraMove: (CameraPosition cameraPosition){
-                latitude = cameraPosition.target.latitude;
-                longitude = cameraPosition.target.longitude;
-                print("$latitude : $longitude");
-              },
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 193,
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/logo/logo2.png',
-                      width: 58.5,
-                      height: 95,
-                    )
-                  ],
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(top: 50, bottom: 30, left: 30, right: 30),
+            child: Stack(
+              children: <Widget>[
+                GoogleMap(
+                  mapType: MapType.normal,
+                  rotateGesturesEnabled: false,
+                  scrollGesturesEnabled: true,
+                  tiltGesturesEnabled: false,
+                  zoomGesturesEnabled: true,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(_currentPosition.latitude , _currentPosition.longitude),
+                    zoom: 18,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller = controller;
+                  },
+                  onCameraMove: (CameraPosition cameraPosition){
+                    latitude = cameraPosition.target.latitude;
+                    longitude = cameraPosition.target.longitude;
+                    print("$latitude : $longitude");
+                  },
                 ),
-              )
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 193,
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/logo/logo2.png',
+                          width: 58.5,
+                          height: 95,
+                        )
+                      ],
+                    ),
+                  )
+                ),
+                Align(
+                  alignment: Alignment(1,0.75),
+                  child: FloatingActionButton(
+                    child: Icon(Icons.my_location),
+                    onPressed: (){
+                      _getCurrentLocation();
+                    },
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Color(0xFF427496) ,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(40))),
+                child:FlatButton(
+                onPressed: () {
+                  _getCurrentLocation();
+                },
+                child: Text(
+                  'ยืนยัน',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                )
+              ),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
-          Navigator.pop(context,[latitude,longitude]);
-        },
-        label: Text('ยืนยัน'),
-      ),
+      
     );
   }
 
